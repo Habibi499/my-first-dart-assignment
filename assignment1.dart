e94cc21ba14b3aba58e5d229c90430db86a3862b
Habibi499 <samuelmungai499@gmail.com>
2023-09-15 20:40:26 +0300

    my first commit

---
+++ assignment1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Samuel Mungai Wanjeri"; //string
+  String description = " is a student at PLP AFRICA Compassion Kenya"; //string
+  int phone = 0719546628; //integer
+  String favfood = " My favourite food is pilau"; //string
+  String email = " My email address is samuelmungai499@gmail.com"; //string
+  print("$name $description. $favfood, $email and my phone number is $phone.");
+}
